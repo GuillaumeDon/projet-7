980c53d2d81d05d95728fc0c0c9da8000bc940be
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-01-25 10:34:37 +0100

    maj eviter redondance des tags

--- scripts/filtres.js
+++ scripts/filtres.js
@@ -1,4 +1,119 @@
 
+// const ingredientsUl = document.querySelector(".container-filtres-tags-tag1 .dropdown-menu ul");
+// const appliancesUl = document.querySelector(".container-filtres-tags-tag2 .dropdown-menu ul");
+// const ustensilsUl = document.querySelector(".container-filtres-tags-tag3 .dropdown-menu ul");
+
+// const allIngredients = new Set();
+// const allAppliances = new Set();
+// const allUstensils = new Set();
+
+// recipes.forEach(recipe => {
+//     recipe.ingredients.forEach(ingredient => {
+//         allIngredients.add(ingredient.ingredient);
+//     });
+
+//     allAppliances.add(recipe.appliance);
+
+//     recipe.ustensils.forEach(ustensil => {
+//         allUstensils.add(ustensil);
+//     });
+// });
+
+
+// function addItemsToList(ulElement, itemValues) {
+//     itemValues.forEach(value => {
+//         const li = document.createElement("li");
+//         li.textContent = value;
+//         ulElement.appendChild(li);
+//     });
+// }
+
+
+// addItemsToList(ingredientsUl, allIngredients);
+// addItemsToList(appliancesUl, allAppliances);
+// addItemsToList(ustensilsUl, allUstensils);
+
+
+
+// const ingredientsInput = document.getElementById("ingredientsDropdown");
+// const logoClose = document.querySelector(".container-filtres-tags-tag1-logo.logoClose");
+
+
+// logoClose.addEventListener("click", () => {
+    
+//     ingredientsInput.value = "";
+// });
+
+// const appliancesInput = document.getElementById("appliancesDropdown");
+// const logoCloseAppliances = document.querySelector(".container-filtres-tags-tag2-logo.logoClose");
+
+// logoCloseAppliances.addEventListener("click", () => {
+//     appliancesInput.value = "";
+// });
+
+
+// const ustensilsInput = document.getElementById("ustensilsDropdown");
+// const logoCloseUstensils = document.querySelector(".container-filtres-tags-tag3-logo.logoClose");
+
+// logoCloseUstensils.addEventListener("click", () => {
+//     ustensilsInput.value = "";
+// });
+
+
+
+// //header filter effacer
+
+// const searchInput = document.getElementById("search");
+// const closeIcon = document.querySelector(".header-form-close");
+
+// closeIcon.addEventListener("click", () => {
+//     searchInput.value = "";
+// });
+
+
+
+
+
+
+
+
+
+// // Récupérez la liste de choix
+// const choiceList = document.querySelector(".container-filtres-choice ul");
+
+// // Ajoutez un gestionnaire d'événements de clic aux éléments de la liste déroulante
+// ingredientsUl.addEventListener("click", (event) => {
+//     if (event.target.tagName === "LI") {
+//         const selectedIngredient = event.target.textContent;
+//         addFilterItem(choiceList, selectedIngredient);
+//     }
+// });
+
+// appliancesUl.addEventListener("click", (event) => {
+//     if (event.target.tagName === "LI") {
+//         const selectedAppliance = event.target.textContent;
+//         addFilterItem(choiceList, selectedAppliance);
+//     }
+// });
+
+// ustensilsUl.addEventListener("click", (event) => {
+//     if (event.target.tagName === "LI") {
+//         const selectedUstensil = event.target.textContent;
+//         addFilterItem(choiceList, selectedUstensil);
+//     }
+// });
+
+// // Fonction pour ajouter un élément à la liste de choix
+// function addFilterItem(ulElement, itemName) {
+//     const li = document.createElement("li");
+//     li.textContent = itemName;
+//     ulElement.appendChild(li);
+// }
+
+
+
+
+
 const ingredientsUl = document.querySelector(".container-filtres-tags-tag1 .dropdown-menu ul");
 const appliancesUl = document.querySelector(".container-filtres-tags-tag2 .dropdown-menu ul");
 const ustensilsUl = document.querySelector(".container-filtres-tags-tag3 .dropdown-menu ul");
@@ -19,7 +134,6 @@ recipes.forEach(recipe => {
     });
 });
 
-
 function addItemsToList(ulElement, itemValues) {
     itemValues.forEach(value => {
         const li = document.createElement("li");
@@ -28,19 +142,14 @@ function addItemsToList(ulElement, itemValues) {
     });
 }
 
-
 addItemsToList(ingredientsUl, allIngredients);
 addItemsToList(appliancesUl, allAppliances);
 addItemsToList(ustensilsUl, allUstensils);
 
-
-
 const ingredientsInput = document.getElementById("ingredientsDropdown");
 const logoClose = document.querySelector(".container-filtres-tags-tag1-logo.logoClose");
 
-
 logoClose.addEventListener("click", () => {
-    
     ingredientsInput.value = "";
 });
 
@@ -51,7 +160,6 @@ logoCloseAppliances.addEventListener("click", () => {
     appliancesInput.value = "";
 });
 
-
 const ustensilsInput = document.getElementById("ustensilsDropdown");
 const logoCloseUstensils = document.querySelector(".container-filtres-tags-tag3-logo.logoClose");
 
@@ -59,47 +167,45 @@ logoCloseUstensils.addEventListener("click", () => {
     ustensilsInput.value = "";
 });
 
-
-
-//header filter effacer
-
-const searchInput = document.getElementById("search");
-const closeIcon = document.querySelector(".header-form-close");
-
-closeIcon.addEventListener("click", () => {
-    searchInput.value = "";
-});
-
-
-
-
-
-
-
-
-
 // Récupérez la liste de choix
 const choiceList = document.querySelector(".container-filtres-choice ul");
+
+// Fonction pour vérifier si un élément existe déjà dans la liste de choix
+function isItemInList(ulElement, itemName) {
+    const items = ulElement.querySelectorAll("li");
+    for (const item of items) {
+        if (item.textContent === itemName) {
+            return true; // L'élément existe déjà dans la liste
+        }
+    }
+    return false; // L'élément n'existe pas dans la liste
+}
 
 // Ajoutez un gestionnaire d'événements de clic aux éléments de la liste déroulante
 ingredientsUl.addEventListener("click", (event) => {
     if (event.target.tagName === "LI") {
         const selectedIngredient = event.target.textContent;
-        addFilterItem(choiceList, selectedIngredient);
+        if (!isItemInList(choiceList, selectedIngredient)) {
+            addFilterItem(choiceList, selectedIngredient);
+        }
     }
 });
 
 appliancesUl.addEventListener("click", (event) => {
     if (event.target.tagName === "LI") {
         const selectedAppliance = event.target.textContent;
-        addFilterItem(choiceList, selectedAppliance);
+        if (!isItemInList(choiceList, selectedAppliance)) {
+            addFilterItem(choiceList, selectedAppliance);
+        }
     }
 });
 
 ustensilsUl.addEventListener("click", (event) => {
     if (event.target.tagName === "LI") {
         const selectedUstensil = event.target.textContent;
-        addFilterItem(choiceList, selectedUstensil);
+        if (!isItemInList(choiceList, selectedUstensil)) {
+            addFilterItem(choiceList, selectedUstensil);
+        }
     }
 });
 
@@ -109,3 +215,12 @@ function addFilterItem(ulElement, itemName) {
     li.textContent = itemName;
     ulElement.appendChild(li);
 }
+
+//header filter effacer
+
+const searchInput = document.getElementById("search");
+const closeIcon = document.querySelector(".header-form-close");
+
+closeIcon.addEventListener("click", () => {
+    searchInput.value = "";
+});
