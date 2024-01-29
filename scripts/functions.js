0f61cb51206fc9b8b9e915eafce90a2510622aeb
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-01-29 20:03:25 +0100

    modifs mineurs scss

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,12 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const searchInput = document.getElementById("search");
-    const closeIcon = document.querySelector(".header-form-close");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const searchInput = document.getElementById("search");
+//     const closeIcon = document.querySelector(".header-form-close");
 
-    searchInput.addEventListener("input", () => {
-        if (searchInput.value.trim() !== "") {
-            closeIcon.classList.remove("hidden");
-        } else {
-            closeIcon.classList.add("hidden");
-        }
-    });
+//     searchInput.addEventListener("input", () => {
+//         if (searchInput.value.trim() !== "") {
+//             closeIcon.classList.remove("hidden");
+//         } else {
+//             closeIcon.classList.add("hidden");
+//         }
+//     });
+// });
+
+
+const searchInput = document.getElementById("search");
+const closeIcon = document.querySelector(".logoCloseHeader");
+
+// Fonction pour gérer la visibilité de l'icône
+function handleIconVisibility() {
+    if (searchInput.value.trim() !== "") {
+        closeIcon.classList.remove("hidden");
+    } else {
+        closeIcon.classList.add("hidden");
+    }
+}
+
+searchInput.addEventListener("input", handleIconVisibility);
+
+// Écouter l'événement "click" sur l'icône de fermeture
+closeIcon.addEventListener("click", () => {
+    searchInput.value = ""; // Efface le contenu de l'input
+    closeIcon.classList.add("hidden"); // Masque l'icône
 });
