7d2e6efc929aa15872c0139e22a730d833c2f6b6
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-02-06 18:42:08 +0100

    MAJ filtre barre de recherche principale

--- scripts/filtres2.js
+++ scripts/filtres2.js
@@ -510,18 +510,57 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const recipesContainer = document.getElementById('recipe-cards');
         recipesContainer.innerHTML = ''; // Vide le conteneur pour les nouveaux résultats
-
         filteredRecipes.forEach(recipe => {
-            const recipeElement = document.createElement('div');
-            recipeElement.classList.add('recipe');
-
-            const recipeContent = `
-                <h2>${recipe.name}</h2>
-                <p>${recipe.description}</p>
-            `;
-            recipeElement.innerHTML = recipeContent;
-
-            recipesContainer.appendChild(recipeElement);
+            const recipeCard = document.createElement('div');
+            recipeCard.classList.add('container-recipe-cards-item');
+        
+            const image = document.createElement('img');
+            image.classList.add('container-recipe-cards-item-img');
+            image.src = "./assets/pictures/recipes/" + recipe.image;
+            recipeCard.appendChild(image);
+        
+            const containerTime = document.createElement('div');
+            containerTime.classList.add('container-recipe-cards-item-containerTime');
+            recipeCard.appendChild(containerTime);
+        
+            const time = document.createElement('p');
+            time.classList.add('container-recipe-cards-item-containerTime-time');
+            time.textContent = `${recipe.time} min`;
+            containerTime.appendChild(time);
+        
+            const title = document.createElement('h2');
+            title.classList.add('container-recipe-cards-item-title');
+            title.textContent = recipe.name;
+            recipeCard.appendChild(title);
+        
+            const secondTitle = document.createElement('h4');
+            secondTitle.classList.add('container-recipe-cards-item-secondTitle');
+            secondTitle.textContent = `Recette`;
+            recipeCard.appendChild(secondTitle);
+        
+            const description = document.createElement('p');
+            description.classList.add('container-recipe-cards-item-description');
+            description.textContent = recipe.description; // Assurez-vous d'avoir une fonction pour tronquer la description si nécessaire
+            recipeCard.appendChild(description);
+        
+            const thirdTitle = document.createElement('h4');
+            thirdTitle.classList.add('container-recipe-cards-item-thirdTitle');
+            thirdTitle.textContent = `Ingrédients`;
+            recipeCard.appendChild(thirdTitle);
+        
+            const ingredientsList = document.createElement('ul');
+            ingredientsList.classList.add('container-recipe-cards-item-ingredientsList');
+            recipe.ingredients.forEach(ingredient => {
+                const ingredientItem = document.createElement('li');
+                ingredientItem.classList.add('container-recipe-cards-item-ingredientsList-item');
+        
+                const ingredientText = `${ingredient.ingredient}: ${ingredient.quantity || ''} ${ingredient.unit || ''}`;
+                ingredientItem.innerHTML = ingredientText;
+                ingredientsList.appendChild(ingredientItem);
+            });
+            recipeCard.appendChild(ingredientsList);
+        
+            recipesContainer.appendChild(recipeCard); // Assurez-vous que `recipesContainer` est correctement sélectionné avant cette opération
         });
     }
 });
