003951a5eed7c29fe2ac64130ad00354d81b1359
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-02-26 10:02:49 +0100

    refactorisation cards

--- scripts/filtres.js
+++ scripts/filtres.js
@@ -411,6 +411,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 updateRecipesCount(filteredRecipes.length);   // Efface le message d'erreur s'il y avait un précédent
             }
 
+            displayRecipeCards(filteredRecipes);
 
             function updateRecipesCount(count) {
                 const recipesCountElement = document.querySelector('.container-filtres-results-number');
@@ -418,71 +419,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
 
-        const recipesContainer = document.getElementById('recipe-cards');
-        recipesContainer.innerHTML = ''; // Vide le conteneur pour les nouveaux résultats
-        filteredRecipes.forEach(recipe => {
-            const recipeCard = document.createElement('div');
-            recipeCard.classList.add('container-recipe-cards-item');
-        
-            const image = document.createElement('img');
-            image.classList.add('container-recipe-cards-item-img');
-            image.src = "./assets/pictures/recipes/" + recipe.image;
-            recipeCard.appendChild(image);
-        
-            const containerTime = document.createElement('div');
-            containerTime.classList.add('container-recipe-cards-item-containerTime');
-            recipeCard.appendChild(containerTime);
-        
-            const time = document.createElement('p');
-            time.classList.add('container-recipe-cards-item-containerTime-time');
-            time.textContent = `${recipe.time} min`;
-            containerTime.appendChild(time);
-        
-            const title = document.createElement('h2');
-            title.classList.add('container-recipe-cards-item-title');
-            title.textContent = recipe.name;
-            recipeCard.appendChild(title);
-        
-            const secondTitle = document.createElement('h4');
-            secondTitle.classList.add('container-recipe-cards-item-secondTitle');
-            secondTitle.textContent = `Recette`;
-            recipeCard.appendChild(secondTitle);
-        
-            const description = document.createElement('p');
-            description.classList.add('container-recipe-cards-item-description');
-            description.textContent = recipe.description; 
-            recipeCard.appendChild(description);
-        
-            const thirdTitle = document.createElement('h4');
-            thirdTitle.classList.add('container-recipe-cards-item-thirdTitle');
-            thirdTitle.textContent = `Ingrédients`;
-            recipeCard.appendChild(thirdTitle);
-        
-            const ingredientsList = document.createElement('ul');
-            ingredientsList.classList.add('container-recipe-cards-item-ingredientsList');
-            recipe.ingredients.forEach(ingredient => {
-                const ingredientItem = document.createElement('li');
-                ingredientItem.classList.add('container-recipe-cards-item-ingredientsList-item');
-        
-                // const ingredientText = `${ingredient.ingredient}: ${ingredient.quantity || ''} ${ingredient.unit || ''}`;            const ingredientText = `${ingredient.ingredient}<br><span class="grey-title">${ingredient.quantity || '-'} ${ingredient.unit || ''} </span>`;
-                const ingredientText = `${ingredient.ingredient}<br><span class="grey-title">${ingredient.quantity || '-'} ${ingredient.unit || ''} </span>`;
-
-                ingredientItem.innerHTML = ingredientText;
-                ingredientsList.appendChild(ingredientItem);
-            });
-            recipeCard.appendChild(ingredientsList);
-        
-            recipesContainer.appendChild(recipeCard);
-            const recipesCountElement = document.querySelector('.container-filtres-results-number');
-recipesCountElement.textContent = filteredRecipes.length; 
-
-        });
-    }
-
-
-
-
-});
+};
 
 
 
@@ -539,3 +476,4 @@ recipesCountElement.textContent = filteredRecipes.length;
 // } else {
 //     filteredRecipes = recipes; 
 // }
+})
